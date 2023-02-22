1cc754b01b8e5a57f2dc285efaf0dd14949c590f
Makova Nataliya <116758787+Crot2050@users.noreply.github.com>
2023-02-22 22:26:20 +0300

    Update 16.8.2.py

--- 16.8.2.py
+++ 16.8.2.py
@@ -21,3 +21,28 @@ class Circle:
 
     def get_circle(self):
         return 3.14 * self.r ** 2
+    
+rect1 = Rectangel(2, 4)
+rect2 = Rectangel(4, 6)
+print(rect1.get_area(),
+      rect2.get_area())
+
+square1 = Square(5)
+square2 = Square(6)
+print(square1.get_area_square(),
+      square2.get_area_square())
+
+circle1 = Circle(7)
+circle2 = Circle(8)
+print(circle1.get_circle(),
+      circle2.get_circle())
+
+figures = (rect1, rect2, square1, square2, circle1, circle2)
+for figure in figures:
+    if isinstance(figure, Square):
+        print("Площадь квадрата = ", figure.get_area_square())
+    elif isinstance(figure, Circle):
+        print("Площадь круга = ", figure.get_circle())
+    else:
+        print("Площадь прямоугольника = ", figure.get_area())
+
